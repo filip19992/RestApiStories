95efa6898c3365e61b1580e98b787792d5ce3e79
filip19992 <filipwlodarczyk99@gmail.com>
2022-07-16 15:56:23 +0200

    Added tests to PasswordValidator

---
+++ src/test/java/pl/filipwlodarczyk/stronaZeZdejciami/registration/PasswordValidatorTest.java
@@ -0,0 +1,49 @@
+package pl.filipwlodarczyk.stronaZeZdejciami.registration;
+
+import org.junit.Test;
+import pl.filipwlodarczyk.stronaZeZdejciami.registration.PasswordValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PasswordValidatorTest {
+    @Test
+    public void shouldValidateWhenPasswordWithFirstUppercaseGiven() throws Exception {
+
+        PasswordValidator.validatePasswordResponse validatePasswordResponse = PasswordValidator.validatePassword("Password");
+
+        boolean validated = validatePasswordResponse.isValidated();
+
+        assertTrue(validated);
+    }
+
+    @Test
+    public void shouldNotValidateWhenPasswordWithFirstLowerCaseGiven() {
+
+        PasswordValidator.validatePasswordResponse validatePasswordResponse = PasswordValidator.validatePassword("password");
+
+        boolean validated = validatePasswordResponse.isValidated();
+
+        assertFalse(validated);
+    }
+
+    @Test
+    public void shouldNotValidateWhenPasswordTooLongIsGiven() {
+        //more than 20 letters given
+        PasswordValidator.validatePasswordResponse validatePasswordResponse = PasswordValidator.validatePassword("passasdasdasdasdasdasdword");
+
+        boolean validated = validatePasswordResponse.isValidated();
+
+        assertFalse(validated);
+    }
+
+    @Test
+    public void shouldNotValidateWhenPasswordTooShortIsGiven() {
+        //less than 7 letters given
+        PasswordValidator.validatePasswordResponse validatePasswordResponse = PasswordValidator.validatePassword("sdword");
+
+        boolean validated = validatePasswordResponse.isValidated();
+
+        assertFalse(validated);
+    }
+}
