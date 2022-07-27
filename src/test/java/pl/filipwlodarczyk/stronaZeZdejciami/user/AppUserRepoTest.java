b06a4585281e57d0fb19f1a82eede83f2a0c9ef3
filip19992 <filipwlodarczyk99@gmail.com>
2022-07-27 19:39:55 +0200

    Wrote test for AppUserRepo.existsByUsername

---
+++ src/test/java/pl/filipwlodarczyk/stronaZeZdejciami/user/AppUserRepoTest.java
@@ -0,0 +1,38 @@
+package pl.filipwlodarczyk.stronaZeZdejciami.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import pl.filipwlodarczyk.stronaZeZdejciami.role.Role;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AppUserRepoTest {
+
+    @Autowired
+    private AppUserRepo underTest;
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void itShouldReturnTrueIfExistsByUsername() {
+        //given
+        String username = "Filip";
+        AppUser appUser = new AppUser(
+                "Filip",
+                "Password",
+                20,
+                "ROLE_USER");
+
+        underTest.save(appUser);
+
+        //when
+        Boolean result = underTest.existsByUsername(username);
+
+        //then
+        assertEquals(true, result);
+    }
+}
